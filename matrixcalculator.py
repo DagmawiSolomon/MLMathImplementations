9bcb28026fd977310fc4bafd6ab29d436a7ea202
Dagmawi Solomon <02dagmawisolomon@gmail.com>
2024-07-06 13:33:33 +0300

    feat:add matrix calculator class

---
+++ matrixcalculator.py
@@ -0,0 +1,18 @@
+class MatrixCalculator:
+    def __init__(self, arr):
+        self.array = arr
+
+    def multiply(self):
+        pass
+
+    def addition(self):
+        pass
+
+    def subtraction(self):
+        pass
+
+    def division(self):
+        pass
+
+    def determinent(self):
+        pass
