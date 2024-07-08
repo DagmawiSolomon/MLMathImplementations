54be8a5141486dad595c5b6825fbd59ea731a717
dagmawiSolomon <02dagmawisolomon@gmail.com>
2024-07-08 03:52:59 +0300

    move to own directory

---
+++ matrixoperations/matrixoperations.py
@@ -0,0 +1,29 @@
+class MatrixOperations:
+    def __init__(self):
+        pass
+        
+    def matrix(self,arr):
+        pass
+    def multiply(self):
+        pass
+
+    def addition(self):
+        pass
+
+    def subtraction(self):
+        pass
+
+    def division(self):
+        pass
+
+    def determinent(self):
+        pass
+
+
+
+
+# import matrixoperations as matrix
+# x = mp.matrix([0,1,2])
+# y = mp.matrix([0,1,2])
+# x + y, x-y, x*y, x/y
+
