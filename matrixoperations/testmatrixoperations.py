8785e6ef0cad93e65aba52e75aaff7d3a4534610
dagmawiSolomon <02dagmawisolomon@gmail.com>
2024-07-08 04:29:20 +0300

    feat:add basic tests for different initializations

--- matrixoperations/testmatrixoperations.py
+++ matrixoperations/testmatrixoperations.py
@@ -3,6 +3,27 @@ from matrixoperations import MatrixOperations
 class MatrixOperationsTestCase(unittest.TestCase):
     def setUp(self):
         self.mp = MatrixOperations
+        
+    def test_init_with_mixed_elements(self):
+        with self.assertRaises(ValueError):
+            self.mp(["a", 1, 2])
+    def test_init_with_non_list_input(self):
+        with self.assertRaises(TypeError):
+            self.mp(1)
+    def test_init_with_numeric_elements(self):
+        self.mp([0,1])
+        self.mp([-1,2])
+        large_value = 458**58
+        self.mp([1,large_value])
+        
+    def test_init_with_float_elements(self):
+        self.mp([0.5,1.6])
+        self.mp([-0.1,2.4])
+        small_value = 2/(458**58)
+        self.mp([1,small_value])
+    def test_init_with_empty_list(self):
+        self.mp([])
+    
     
             
 if __name__ == '__main__':
