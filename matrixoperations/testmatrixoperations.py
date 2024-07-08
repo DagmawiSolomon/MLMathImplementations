deeff7f3d8f7d9400ca82cfdc4745d46a724dc04
dagmawiSolomon <02dagmawisolomon@gmail.com>
2024-07-08 04:15:36 +0300

    fix(test_matrix):remove the implementaion since the function matrix was removed

--- matrixoperations/testmatrixoperations.py
+++ matrixoperations/testmatrixoperations.py
@@ -2,16 +2,8 @@ import unittest
 from matrixoperations import MatrixOperations
 class MatrixOperationsTestCase(unittest.TestCase):
     def setUp(self):
-        self.mp = MatrixOperations()
-        
-    def test_matrix(self):
-        self.mp.matrix([0,1,2])
-        self.mp.matrix([[0,1,2],[3,4,5]])
-        with self.assertRaises(TypeError):
-            self.mp.matrix(0)
-        with self.assertRaises(ValueError):
-            self.mp.matrix([0,1,2,"3"])
-            
+        self.mp = MatrixOperations
+    
             
 if __name__ == '__main__':
     unittest.main()
