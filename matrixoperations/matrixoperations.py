cd8beda894ac632c87e683bcd2eac8afba9fbd16
dagmawiSolomon <02dagmawisolomon@gmail.com>
2024-07-09 04:33:54 +0300

    fix:implement better way to flatten an array

--- matrixoperations/matrixoperations.py
+++ matrixoperations/matrixoperations.py
@@ -1,19 +1,26 @@
+from itertools import chain
+
 class MatrixOperations:
     def __init__(self, matrix):
         self.matrix = matrix
         self.validate_matrix()
-        
+       
+                
     def flatten(self):
-        pass
+        if len(self.matrix) > 0 and isinstance(self.matrix[0], list):
+            return list(chain.from_iterable(self.matrix))
+        else:
+            return self.matrix
+
     def validate_matrix(self):
-        if not isinstance(self.matrix, list):
+        
+        if not isinstance(self.flatten(), list):
             raise TypeError("Input must be a list")
-        for element in self.matrix:
+        for element in self.flatten():
             if not isinstance(element, (int,float)):
                 raise ValueError("All elements in the list be a number type(int or float)")
             
-    def flatten(self,arr):
-        pass
+    
     def multiply(self):
         pass
 
